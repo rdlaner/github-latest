594132ab6d8b64cf0c1db0f5801cd034c3fb83d5
Dustin L <r.dustin.lane@gmail.com>
2018-10-17 22:18:13 -0700

    Completed script

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
+"""Github API test script"""
 import sys
-import json
-
 import requests
 
-# Use Like python githubber.py JASchilz
-# (or another user name)
 
 if __name__ == "__main__":
     username = sys.argv[1]
+    response = requests.get(f"https://api.github.com/users/{username}/events")
+    events = response.json()
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
-
-    print("COMPLETE THE TODOs")
-    
-
-
+    print(events[0]["created_at"])
